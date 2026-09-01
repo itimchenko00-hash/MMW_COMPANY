96a2537b367653a0d5cdbad20044e0ea79524c25
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-01 07:42:09 +0300

    Add public project portfolio routes

--- src/server.js
+++ src/server.js
@@ -15,6 +15,16 @@ app.get('/aladin-pokupateli', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/aladin-buyers.html'));
 });
 
+// MMW-COMPANY project portfolio: ALADIN + four distinct demonstration projects.
+app.get('/projects', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/projects-v2.html'));
+});
+
+// Friendly alias for the portfolio.
+app.get('/portfolio', (req, res) => {
+  res.redirect('/projects');
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`MMW-COMPANY listening on ${PORT}`);
 });

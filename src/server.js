b261469629cd6bed91c32182ce7bf3d9fb06938c
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-01 04:21:57 +0300

    Add MMW-COMPANY web server

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 10000;
+app.use(express.static(path.join(__dirname, '../public')));
+app.get('*', (_req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+app.listen(PORT, () => console.log(`MMW-COMPANY listening on ${PORT}`));
